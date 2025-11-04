006f603a4643d9d7c473f680f9f0b43241123c1b
Tomasz Wilk <tomasz.wilk@infotech.edu.pl>
2025-11-04 13:49:19 +0100

    Basic Movement

---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController2D : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 _moveDirection;
+
+    public InputActionReference move;
+    public InputActionReference fire;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+    }
+
+    private void OnEnable()
+    {
+        fire.action.started += Fire;
+    }
+
+    private void OnDisable()
+    {
+        fire.action.started -= Fire;
+    }
+
+    private void Fire(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Fired");
+    }
+}
